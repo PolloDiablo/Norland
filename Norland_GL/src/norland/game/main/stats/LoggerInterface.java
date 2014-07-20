6e5058e4faa6bc62794430b9b1c9ef390a03eb54
Tony <akroeker@tutelatechnologies.com>
2014-07-19 17:49:20 -0700

    Updated stats code to have support for exporting as json to server. Still in test-mode.

--- Norland_GL/src/norland/game/main/stats/LoggerInterface.java
+++ Norland_GL/src/norland/game/main/stats/LoggerInterface.java
@@ -45,6 +45,6 @@ public interface LoggerInterface {
     /**
      * Upload all logged level interactions.
      */
-    public void uploadLoggedStats();
+    public void uploadLoggedStats(Context context);
 
 }
---
+++ Norland_GL/src/norland/game/main/stats/StatsBundle.java
@@ -0,0 +1,15 @@
+package norland.game.main.stats;
+
+import java.util.Stack;
+
+/**
+ * Used to send up information to the server.
+ * 
+ * @author Tony
+ */
+public final class StatsBundle {
+
+    public StatsBundle(final Stack<LevelInteraction> loggedInteractions) {
+        // TODO Auto-generated constructor stub
+    }
+}
