fd2b7be8674b399fe2f782d8788cf9d3c44f3d77
Tony <akroeker@tutelatechnologies.com>
2014-07-19 20:06:23 -0700

    New norland export format project.

---
+++ NorlandExportFormat/src/ca/casualt/norland/exportformat/StatsBundle.java
@@ -0,0 +1,31 @@
+package ca.casualt.norland.exportformat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Used to send up information to the server.
+ * 
+ * @author Tony
+ */
+public final class StatsBundle {
+
+    private final List<LevelInteraction> loggedInteractions;
+
+    /**
+     * The StatsBundle.
+     * 
+     * @param loggedInteractions
+     */
+    public StatsBundle(final Stack<LevelInteraction> loggedInteractions) {
+        this.loggedInteractions = new ArrayList<LevelInteraction>(loggedInteractions);
+    }
+
+    /**
+     * @return the loggedInteractions
+     */
+    public List<LevelInteraction> getLoggedInteractions() {
+        return loggedInteractions;
+    }
+}
